49db55befc2b8cddbd300e854ae35e4da7c83d7a
marceloquirogasaavedra <120491745+marceloquirogasaavedra@users.noreply.github.com>
2024-11-29 06:47:34 -0400

    Update PedidoModiDTO.java

--- src/main/java/com/sistema/pos/dto/PedidoModiDTO.java
+++ src/main/java/com/sistema/pos/dto/PedidoModiDTO.java
@@ -1,1 +1,14 @@
 
+package com.sistema.pos.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class PedidoModiDTO {
+
+    private String descripcion;
+    private Long id_cliente;
+    private List<Pedido_ProductoDTO> detalle;
+}
